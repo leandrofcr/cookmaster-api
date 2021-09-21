a1eaacf03ba2f2d1a2e1417748e4fc87b0cb7fd6
Leandro Reis <lleandrofr@gmail.com>
2021-09-21 09:42:49 -0300

    Initial commit

---
+++ src/api/models/connection.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require('mongodb');
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster'; /* for local testing */
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster'; /* for github evaluator */
+
+const DB_NAME = 'Cookmaster';
+
+let db = null;
+
+const getConnection = () => (db
+    ? Promise.resolve(db)
+    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+    .then((conn) => {
+      db = conn.db(DB_NAME);
+      return db;
+    }));
+
+module.exports = { getConnection };
