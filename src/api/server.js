d4ed14486a2f5971ad476cbf1919504269ca5cf7
Leandro Reis <lleandrofr@gmail.com>
2021-09-23 14:55:27 -0300

    fix: removes PORT env variable to fix evaluator step on github

--- src/api/server.js
+++ src/api/server.js
@@ -1,6 +1,5 @@
-require('dotenv').config();
 const app = require('./app');
 
-const { PORT } = process.env || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Online at the port ${PORT}`));
