f1062fbe456da61969b4093c2a422fc8a02cf2f6
Leandro Reis <lleandrofr@gmail.com>
2021-09-24 13:28:37 -0300

    test: check the format of the email entered before logging in - at loginService.js

--- src/api/services/loginService.js
+++ src/api/services/loginService.js
@@ -1,8 +1,15 @@
-const { checkLoginEntries, verifyLoginData } = require('../validations/loginValidations');
+const {
+  checkLoginEntries,
+  verifyLoginData,
+  checkEmailFormat,
+} = require('../validations/loginValidations');
 
 const checkLogin = async (email, password) => {
   const loginData = checkLoginEntries(email, password);
   if (loginData.message) return { message: loginData.message };
+
+  const emailFormat = checkEmailFormat(email, password);
+  if (emailFormat.message) return { message: emailFormat.message };
 
   const user = await verifyLoginData(email, password);
   if (user.message) return { message: user.message };
